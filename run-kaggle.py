860d09b9d2f9580f9a6086005b3fc0d53a54103d
hwnginsoict <thoquyenv@gmail.com>
2024-10-21 10:44:39 +0700

    add

--- run-kaggle.py
+++ run-kaggle.py
@@ -1,6 +1,7 @@
 # Import necessary libraries
 from algorithms.inferv1 import INFERV1
-from algorithms.td_daco import TD_DACO
+# from algorithms.td_daco import 
+from algorithms.inferv1 import INFER_V2
 from problemtd import ProblemTD
 
 import csv
@@ -9,7 +10,7 @@ import numpy as np
 import time
 
 # Define the input directory where the CSV files are stored in the Kaggle environment
-input_dir = '/kaggle/input/dvrptw/dvrptw/100/'  # Change this to the correct dataset path in Kaggle
+input_dir = '/kaggle/working/dvrpsd/data/dvrptw/200/'  # Change this to the correct dataset path in Kaggle
 
 # List of files to process
 # file_list = ['h100r201.csv', 'h100r202.csv', 'h100r203.csv', 'h100r204.csv', 'h100r205.csv', 'h100r206.csv']
@@ -23,33 +24,34 @@ input_dir = '/kaggle/input/dvrptw/dvrptw/100/'  # Change this to the correct dat
 file_list = [ 'h100c201.csv', 'h100rc101.csv', 'h100rc201.csv']
 
 # Output file path
-output_file_path = '/kaggle/working/new_final_csv_latest.csv'
+output_file_path = '/kaggle/working/result2110.csv'
 
 file_path = 'finetune_static.csv'
 if not os.path.exists(file_path):
     with open(file_path, mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(['file_name', 'seed', 'ni sta', 'ni dyn', 'td', 'rd', 'time'])  # Add seed column to the header
+        writer.writerow(['file_name', 'seed', 'td', 'rd', 'time'])  # Add seed column to the header
 
 # Start measuring time
 # start_time = time.time()
 
 # Loop over the seed values and files
 for file_name in file_list:
-    for seed in [1,2,3]:
-        for nista in [10,20,50,100]:
-            for nidyn in [5,10,20,30]:
+    for type in ['C1', 'R1', 'RC1', 'C2', 'R2', 'RC2']:
+        for i in range(1,11):
+            file_name = 'h200'+ type + str(i) + '.csv'
+            for seed in range(1, 6):
                 np.random.seed(seed)  # Set the seed
 
                 # Solve the problem and get the result
-                problem1 = ProblemTD("/kaggle/input/dvrptw/dvrptw/100/" + file_name)
+                problem1 = ProblemTD(input_dir + file_name)
                 start_time = time.time()
-                haco = TD_DACO(problem1)
+                haco = INFER_V2(problem1)
 
-                haco.num_ants_static = nista
-                haco.max_iteration_static = nista
-                haco.num_ants_dynamic = nidyn
-                haco.max_iteration_dynamic = nidyn
+                haco.num_ants_static = 50
+                haco.max_iteration_static = 50
+                haco.num_ants_dynamic = 30
+                haco.max_iteration_dynamic = 30
 
                 haco.run()
 
@@ -61,7 +63,7 @@ for file_name in file_list:
                 # Write results to the CSV file
                 with open(file_path, mode='a', newline='') as file:
                     writer = csv.writer(file)
-                    writer.writerow([file_name, seed, nista,nidyn, result[0], result[1], running_time])  # Include seed in the result
+                    writer.writerow([file_name, seed, result[0], result[1], running_time])  # Include seed in the result
 
 
 print("Results have been written to", output_file_path)
