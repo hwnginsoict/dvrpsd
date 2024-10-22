1643d1d46a0688c5eccb1d613b2f0fe2337de6d1
hwnginsoict <thoquyenv@gmail.com>
2024-10-22 11:30:33 +0700

    add

--- run-kaggle.py
+++ run-kaggle.py
@@ -43,7 +43,7 @@ if not os.path.exists(file_path):
 # Loop over the seed values and files
 
 # for type in ['C1', 'R1', 'RC1', 'C2', 'R2', 'RC2']:
-for type in ['RC1']:
+for type in ['RC2']:
     for i in range(1,11):
         file_name = 'h' + str(n) + '00' + type + '_' + str(n) +'_' + str(i) + '.csv'
         for seed in range(1, 4):
