e29049a8972a77847681e992de06d43c9dc73aa1
hwnginsoict <thoquyenv@gmail.com>
2024-10-31 15:33:18 +0700

    add

--- run-kaggle.py
+++ run-kaggle.py
@@ -41,7 +41,7 @@ if not os.path.exists(file_path):
 
 
 for type in ['C1']:#, 'R1', 'RC1', 'C2', 'R2', 'RC2']:
-    for n in [1,2,4]:
+    for n in [2,4]:
         for i in range(1,3):
             input_dir = '/kaggle/working/dvrpsd/data/dvrptw/' + str(n) + '00/'
             file_name = 'h' + str(n) + '00' + type + '_' + str(n) +'_' + str(i) + '.csv'
